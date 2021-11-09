563ce3bb32ecb845e0113b816f71ae507c7d2384
dylan fair <dylanfair11@gmail.com>
2021-11-08 22:41:50 -0600

    added database to server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,18 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '1515',
+        database: 'election'
+    },
+    console.log('connected to the election database.')
+);
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 app.use((req, res) => {
   res.status(404).end();
 });
